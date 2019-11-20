4563497990b65d2b26734602e8f14af0c115b717
Osman KHODER <osmankhoder7@gmail.com>
2019-11-20 19:36:15 +0200

    Add Virtual Driver Test.

---
+++ tests/virtual-driver/js/main.js
@@ -0,0 +1,40 @@
+var userName = 'user' + Math.floor((Math.random() * 1000) + 1);
+var driverLocation = 'lat: ' + Math.floor((Math.random() * 1000) + 1) + ", log: " + Math.floor((Math.random() * 1000) + 1);
+
+var socket = io('http://localhost:9092/driver', {
+    transports: ['polling', 'websocket']
+});
+socket.on('connect', function () {
+    output('<span class="connect-msg">The driver has connected with the server. Username: ' + userName + '</span>');
+});
+socket.on('get_location', function (data) {
+    console.log('Received message', data);
+    output('<span class="username-msg">' + data.userName + ':</span> ' + data.message);
+});
+socket.on('disconnect', function () {
+    output('<span class="disconnect-msg">The client has disconnected!</span>');
+});
+socket.on('reconnect_attempt', (attempts) => {
+    console.log('Try to reconnect at ' + attempts + ' attempt(s).');
+});
+
+function sendDisconnect() {
+    socket.disconnect();
+}
+
+function sendLocation() {
+    var jsonObject = {userName: userName, message: driverLocation, actionTime: new Date()};
+    socket.emit('get_location', jsonObject);
+}
+
+function output(message) {
+    var currentTime = "<span class='time'>" + moment().format('HH:mm:ss.SSS') + "</span>";
+    var element = $("<div>" + currentTime + " " + message + "</div>");
+    $('#console').prepend(element);
+}
+
+$(document).keydown(function (e) {
+    if (e.keyCode == 13) {
+        $('#send').click();
+    }
+});
